6ff93a00b866c07d5d9d4ee1782f47ee0274ec9e
Jamile Matos dos Reis <140525086+JamileReis@users.noreply.github.com>
2024-07-09 11:03:18 -0300

    classe

--- src/main/java/com/screenMatch/ScreenMatch/ScreenMatchApplication.java
+++ src/main/java/com/screenMatch/ScreenMatch/ScreenMatchApplication.java
@@ -1,15 +1,12 @@
 package com.screenMatch.ScreenMatch;
-
-import com.screenMatch.ScreenMatch.models.DadosSerie;
-import com.screenMatch.ScreenMatch.service.ConsumoAPI;
-import com.screenMatch.ScreenMatch.service.ConverterDados;
+import com.screenMatch.ScreenMatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ScreenMatchApplication implements CommandLineRunner {
-
 
 
 	public static void main(String[] args) {
@@ -18,13 +15,8 @@ public class ScreenMatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoAPI = new ConsumoAPI();
-		var json  = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-		System.out.println(json);
-
-		ConverterDados conversor = new ConverterDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 
 	}
 }
