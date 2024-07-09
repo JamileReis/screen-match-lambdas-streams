300e549a9af38dd9e0cdb6d7654949377e41221e
Jamile Matos dos Reis <140525086+JamileReis@users.noreply.github.com>
2024-07-09 11:03:04 -0300

    records

--- src/main/java/com/screenMatch/ScreenMatch/models/DadosTemporadas.java
+++ src/main/java/com/screenMatch/ScreenMatch/models/DadosTemporadas.java
@@ -1,2 +1,12 @@
-package com.screenMatch.ScreenMatch.models;public record DadosTemporadas() {
+package com.screenMatch.ScreenMatch.models;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosTemporadas(@JsonAlias("Season") Integer numero,
+                             @JsonAlias("Episodes") List<DadosEpisodios> episodios
+                              ) {
 }
